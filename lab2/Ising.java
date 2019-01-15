8b2728bbce314fda176249dfe8c84e6f221e8807
K4liber <janbielecki94@gmail.com>
2019-01-15 11:12:14 +0100

    lab2

---
+++ lab2/Ising.java
@@ -0,0 +1,25 @@
+
+// Check magnetisation in function of B
+public class Ising {
+	
+	public int N = 1000;
+	public double J;
+	public double h;
+	public double B;
+	public **int s;
+	
+	public double energy(int x, int y) {
+		double e = 0;
+		int* n = new int[4][2];
+		if (x > 0 && x < N-1 && y > 0 && y < N-1) {
+			
+		}
+		e-=J*s[i]*s[j];
+		return 
+	}
+	
+	public static void main() {
+		
+	}
+	
+}
